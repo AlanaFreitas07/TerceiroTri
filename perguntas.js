eb21eae39719d44b8855b9dd3fa2bd70765b047a
AlanaFreitas07 <162931346+AlanaFreitas07@users.noreply.github.com>
2024-10-29 17:37:35 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,35 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
-)
-
-criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Qual é a primeira cidade de Paraná?',
+    'É Paranaguá, considerada cidade mãe do Paraná.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Matemática',
+    'Qual a o valor de π',
+    '3,141592653..., mas a aproximação mais utilizada para o valor de π é 3,14.'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'História',
+    'Em que ano começou a segunda guerra mundial?',
+    'Em 1939 até 1945'
+)
+
+criaCartao(
+    'Português',
+    'Qual são os sinonimos de zoofagia?',
+    'Carnivorismo, predação, predatismo, carnivoria, insetívora, piscivoria.'
+)
+
+criaCartao(
+    'Física',
+    'Numa tempestade, devo entrar num carro ou embaixo de uma árvore?',
+    'a orientação é se abrigar imediatamente em um veículo, evitando árvores.'
+)
+
+criaCartao(
+    'Química',
+    'Qual a IUPAC da amoxicilina?',
+    '(2S,5R,6R)-6-[[(2R)-2-amino-2-(4-hydroxyphenyl)acetyl]amino]-3,3-dimethyl-7-oxo-4-thia-1-azabicyclo[3.2.0]heptane-2-carboxylic acid'
 )
